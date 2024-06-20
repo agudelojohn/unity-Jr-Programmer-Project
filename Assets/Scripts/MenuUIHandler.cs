e9148c17e3321a68534fea21e5bee8f0b0564884
John Alejandro Agudelo Holguín <jhbr08@hotmail.com>
2024-06-19 20:02:15 -0500

    Start button working

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -14,11 +15,16 @@ public class MenuUIHandler : MonoBehaviour
     {
         // add code here to handle when a color is selected
     }
-    
+
     private void Start()
     {
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
     }
 }
